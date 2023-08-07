900e4a133256fc9ccce0efd75d1780b751766be8
Sharon Brown <xSharonB030705x@gmail.com>
2023-08-07 11:48:53 -0400

    build added to package.json file

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 require('dotenv').config()
 const morgan = require('morgan')
 const {expressjwt} = require('express-jwt')
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 const path = require("path")
 
 //middleware
