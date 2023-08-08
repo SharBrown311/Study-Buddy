c3a85d56da9da7f08efc64a70e3d89be8c885e55
Sharon Brown <xSharonB030705x@gmail.com>
2023-08-08 19:41:28 -0400

    front end changes

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const path = require("path")
 //middleware
 app.use(express.json())
 app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(express.static(path.join(__dirname, "client", "dist")))
 
 // //CONNECTION TO MONGODB
 mongoose.set('strictQuery', false);
